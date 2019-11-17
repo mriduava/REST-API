a20eb4f68f91b685645565268142714c7cacbf1d
mriduava <mriduava@gmail.com>
2019-11-17 19:32:05 +0100

    add mongolab

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 let mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/friends_api');
+//mongoose.connect('mongodb://localhost/friends_api');
+mongoose.connect('mongodb://mriduapi:Bogra2016@ds029969.mlab.com:29969/mriduapi');
+
 
 mongoose.Promise = Promise;
 
